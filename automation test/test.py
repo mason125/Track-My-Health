52db3b6c5ec3f4ec346a0758033a0b9e55d6a4cc
mason125 <mason.white@snhu.edu>
2018-07-06 19:05:25 -0500

    Cleaned up BP
    BP test script cleaned up

--- automation test/test.py
+++ automation test/test.py
@@ -1,29 +1,38 @@
-from selenium import webdriver
-import time
-path = r'C:\Users\Mason\Desktop\chromedriver.exe'
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+import Login
+#values
+topNum = 120
+botNum = 80
 
-var = webdriver.Chrome(path)
+#login data
+user = Login.login
+driver = user.data()
 
-var.get("http://localhost/PhpProject30/index.php#!/")
+#naviagate to user's BP
+driver.find_element_by_id("bp_btn").click();
+try:
+    wait(driver, 10).until(EC.presence_of_element_located((By.ID, "BP_enter_btn")))
+except:
+    print("error at sub menu")
+finally:
+    driver.find_element_by_id("BP_enter_btn").click()
 
-#enter blood pressure
-var.find_element_by_id('home').click()
-var.find_element_by_id('bp_btn').click()
-var.find_element_by_id('BP_enter_btn').click()
+#enter data
+try:
+    wait(driver, 10).until(EC.presence_of_element_located((By.ID, "topNumber")))
+except:
+    print("error at view")
+finally:
+    driver.find_element_by_id("topNumber").send_keys(topNum)
+    driver.find_element_by_id("botNumber").send_keys(botNum)
+    driver.find_element_by_id("bp_enter").click()
 
-#enter data and submit
-time.sleep(.5)
-topNum = var.find_element_by_id('topNumber')
-botNum = var.find_element_by_id('botNumber')
-topNum.send_keys(150)
-botNum.send_keys(90)
-var.find_element_by_id('bp_enter').click()
-time.sleep(.5)
-
-#alert handler
-txt = var.switch_to.alert
-if(txt.text == "Update Complete!"):
-    print("test passed")
-    txt.accept()
-else:
-    print("test failed!")
+#handel alert
+try:
+    wait(driver, 10).until(EC.alert_is_present())
+except:
+    print("error at alert")
+finally:
+    driver.switch_to.alert.accept()
