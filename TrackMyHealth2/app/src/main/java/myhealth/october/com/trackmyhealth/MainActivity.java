28e493bf2cb99bf916b3c73019a1e62d5345a58a
mason125 <mason.white@snhu.edu>
2018-08-08 01:17:53 -0500

    Native Android App
    Migrating project to Java

---
+++ TrackMyHealth2/app/src/main/java/myhealth/october/com/trackmyhealth/MainActivity.java
@@ -0,0 +1,27 @@
+package myhealth.october.com.trackmyhealth;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        controls();
+    }
+    public void controls() {
+        Button btn = (Button) findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Main_Menu.class));
+            }
+        });
+    }
+}
---
+++ TrackMyHealth2/app/src/main/java/myhealth/october/com/trackmyhealth/Main_Menu.java
@@ -0,0 +1,19 @@
+package myhealth.october.com.trackmyhealth;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+public class Main_Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__menu);
+
+
+    }
+
+}
